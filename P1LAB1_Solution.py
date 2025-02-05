e35741f3eca36afd1fde91983456940e6b9881c9
Anthony Cameron <camerona@faytechcc.edu>
2025-02-05 16:39:19 -0500

    Add files via upload

---
+++ P1LAB1_Solution.py
@@ -0,0 +1,9 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
+
+
+
+
